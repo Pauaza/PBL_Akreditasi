08085bb0cded8929e732a13a5fcfd40926450efe
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-05-15 13:57:02 +0700

    add seeder: table m_level

---
+++ PBL/database/seeders/LevelSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $levels = [
+            ['level_kode' => 'ADM', 'level_nama' => 'admin'],
+            ['level_kode' => 'KPS', 'level_nama' => 'kps'],
+            ['level_kode' => 'KJR', 'level_nama' => 'kajur'],
+            ['level_kode' => 'KJM', 'level_nama' => 'kjm'],
+            ['level_kode' => 'DIR', 'level_nama' => 'direktur'],
+        ];
+
+        DB::table('m_level')->insert($levels);
+    }
+}
